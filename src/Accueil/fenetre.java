b791e8a9db3780399be5aad852a5a38372499566
lucas <lucas.dahmani@edu.ece.fr>
2023-04-05 09:51:25 +0200

    Ajout hachage mot de passe

--- src/Accueil/fenetre.java
+++ src/Accueil/fenetre.java
@@ -4,7 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.UnsupportedEncodingException;
 import java.sql.*;
+import java.security.*;
+import java.security.MessageDigest;
+import java.util.Base64;
+import java.security.Provider;
+import java.security.Security;
+import java.util.Set;
+import javax.xml.*;
 
 
 public class fenetre extends JFrame {
@@ -148,34 +156,54 @@ public class fenetre extends JFrame {
 ///CHANGER LES VALEURS PAR DEFAUT PAR LES VALEURS DU REGISTER
 
     private boolean isValidUser(String username, char[] password) {
-        boolean valid = false;
-
         try {
-            // Établir la connexion avec la base de données
+            // Récupérer le mot de passe haché de l'utilisateur à partir de la base de données
             Connection connection = DriverManager.getConnection(url, login, passwd);
-
-            // Préparer la requête SQL pour vérifier si l'utilisateur existe dans la table "users"
-            String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+            String query = "SELECT password FROM users WHERE username = ?";
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, username);
-            statement.setString(2, String.valueOf(password));
             ResultSet result = statement.executeQuery();
-
-            // Vérifier si l'utilisateur existe dans la table "users"
             if (result.next()) {
-                valid = true;
+                String hashedPasswordFromDB = result.getString("password");
+
+                // Hacher le mot de passe entré par l'utilisateur avec SHA-1
+                MessageDigest md = MessageDigest.getInstance("SHA-1");
+                md.update(new String(password).getBytes("UTF-8"));
+                byte[] digest = md.digest();
+                String hashedInputPassword = bytesToHex(digest); // Utilisez bytesToHex ici
+
+                // Comparer les deux mots de passe hachés
+                if (hashedInputPassword.equalsIgnoreCase(hashedPasswordFromDB)) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
-
-            // Fermer la connexion avec la base de données
-            result.close();
-            statement.close();
-            connection.close();
-        } catch (SQLException e) {
+            else {
+                return false;
+            }
+        } catch (SQLException | NoSuchAlgorithmException | UnsupportedEncodingException e) {
             e.printStackTrace();
+            return false;
         }
-
-        return valid;
     }
+
+
+
+
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int i = 0; i < bytes.length; i++) {
+            int v = bytes[i] & 0xFF;
+            hexChars[i * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[i * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+
     private void displayUserInformation(String username) {
         try {
             // Établir la connexion avec la base de données
@@ -193,7 +221,7 @@ public class fenetre extends JFrame {
             JFrame userFrame = new JFrame("Informations de l'utilisateur");
 
             // Définir la taille de la fenêtre
-            userFrame.setSize(1920 , 1080);
+            userFrame.setSize(1920, 1080);
 
             // Centre la fenêtre au milieu de l'écran
             userFrame.setLocationRelativeTo(null);
@@ -235,38 +263,57 @@ public class fenetre extends JFrame {
         }
     }
 
-//Register pour user
+
     private boolean registerUser(String firstName, String lastName, String email, String username, char[] password) {
         boolean success = false;
-
+        Connection connection = null;
+        PreparedStatement statement = null;
         try {
             // Établir la connexion avec la base de données
-            Connection connection = DriverManager.getConnection(url, login, passwd);
+            connection = DriverManager.getConnection(url, login, passwd);
 
             // Préparer la requête SQL pour insérer un nouvel utilisateur dans la table "users"
             String query = "INSERT INTO users (first_name, last_name, email, username, password) VALUES (?, ?, ?, ?, ?)";
 
+            // Hacher le mot de passe avec SHA-1
+            MessageDigest digest = null;
+            try {
+                digest = MessageDigest.getInstance("SHA-1");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            byte[] hashedPassword = digest.digest(String.valueOf(password).getBytes("UTF-8"));
+            String hashedPasswordHex = bytesToHex(hashedPassword);
             // Exécuter la requête SQL
-            PreparedStatement statement = connection.prepareStatement(query);
+            statement = connection.prepareStatement(query);
             statement.setString(1, firstName);
             statement.setString(2, lastName);
             statement.setString(3, email);
             statement.setString(4, username);
-            statement.setString(5, String.valueOf(password));
+            statement.setString(5, hashedPasswordHex);
             int rows = statement.executeUpdate();
 
             // Vérifier si l'insertion a réussi
             if (rows > 0) {
                 success = true;
             }
-
-            // Fermer la connexion avec la base de données
-            statement.close();
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Fermer la connexion avec la base de données
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-
         return success;
     }
 
