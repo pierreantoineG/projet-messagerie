fbedf1548cd6f7afc143fe2657ad9453b63c6a3e
pierre-antoinegrand <pierreantoine.grand@edu.ece.fr>
2023-04-19 11:25:03 +0200

    essaie

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
 
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        System.out.println("Good good");
         fenetre fenetre = new fenetre();
 
     }
